6c0f7dc85ddcd1242f899e8ee447361af92056b1
Kip Lacy <kipdlacy@gmail.com>
2024-01-18 13:15:24 -0500

    Modified the MergeAdjacents script to be flexible for the number of samples included in the analysis

--- ShortReadGenomes_CrossoverAndLOHDetection/PhaseClean_MergeAdjacents_FlexibleVersion.py
+++ ShortReadGenomes_CrossoverAndLOHDetection/PhaseClean_MergeAdjacents_FlexibleVersion.py
@@ -32,24 +32,48 @@ OutFile.write(PShead[0])
 for i in range(1,len(PShead)):
 	OutFile.write('\t'+PShead[i])
 
+def compare_elements(list1, list2, indices):
+	"""
+	Compares elements of two lists at specified indices.
+	This function is designed to compare elements of list1 and list2 at the indices provided in the 'indices' list.
+	It will only compare up to the length of the shorter list to avoid IndexError in case one list is shorter than the other.
+	indices (list of int): The indices at which to compare elements in the lists.
+	Returns:
+	bool: True if all compared elements are equal, False otherwise.
+	"""
+	for index in indices:
+		# Check if the current index is within the bounds of both lists
+		if index < len(list1) and index < len(list2):
+			# Compare elements at the current index; if any pair doesn't match, return False
+			if list1[index] != list2[index]:
+				return False
+		else:
+			# If the index is out of bounds for either list, stop comparing further
+			break
+	return True
+
 ### Looping, identifying mergeable adjacents, writing out either unmerged or merged
-i=0
+i = 0
 while i < len(PSs):
-	if i+1 < len(PSs):
-		if PSs[i][0]==PSs[i+1][0] and PSs[i][1]==PSs[i+1][1] and PSs[i][8]==PSs[i+1][8] and PSs[i][9]==PSs[i+1][9] and PSs[i][10]==PSs[i+1][10] and PSs[i][11]==PSs[i+1][11]:
-			Run=[]
+	if i + 1 < len(PSs):
+		# Use the compare_elements function to compare PSs[i] and PSs[i+1] at the specified indices
+		if compare_elements(PSs[i], PSs[i+1], [0, 1, 8, 9, 10, 11, 12, 13]):
+			Run = []
 			Run.append(PSs[i])
 			Run.append(PSs[i+1])
-			p=i+2
-			RunGood='True'
-			while RunGood=='True':
+			p = i + 2
+			RunGood = 'True'
+			while RunGood == 'True':
 				if p >= len(PSs):
-					RunGood='False'
-				elif PSs[i][0]==PSs[p][0] and PSs[i][1]==PSs[p][1] and PSs[i][8]==PSs[p][8] and PSs[i][9]==PSs[p][9] and PSs[i][10]==PSs[p][10] and PSs[i][11]==PSs[p][11]:
-					Run.append(PSs[p])
-					p+=1
+					RunGood = 'False'
 				else:
-					RunGood='False'
+					# Again, use the compare_elements function for comparison with PSs[p]
+					if compare_elements(PSs[i], PSs[p], [0, 1, 8, 9, 10, 11, 12, 13]):
+						Run.append(PSs[p])
+						p += 1
+					else:
+						RunGood = 'False'
+			# Writing merged data to OutFile
 			OutFile.write('\n'+PSs[i][0]+'\t'+PSs[i][1]+'\t'+PSs[i][2]+'\t'+Run[-1][3])
 			NumSNPs=0
 			for y in Run:
