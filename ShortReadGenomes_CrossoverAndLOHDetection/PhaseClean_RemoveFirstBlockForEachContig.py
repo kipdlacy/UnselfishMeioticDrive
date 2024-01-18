de830ff658f18fac5e3dbc7a3380416359101244
Kip Lacy <kipdlacy@gmail.com>
2024-01-18 18:33:57 -0500

    Added comments to Remove1stBlkEachContig file

--- ShortReadGenomes_CrossoverAndLOHDetection/PhaseClean_RemoveFirstBlockForEachContig.py
+++ ShortReadGenomes_CrossoverAndLOHDetection/PhaseClean_RemoveFirstBlockForEachContig.py
@@ -2,6 +2,7 @@
 
 ### 2023FEB10
 ### This script is designed to read in a list of inferred phase switches, and remove any phase blocks that are the first on their contig
+### This is useful in cases where you don't want to count the beginning of the contig as a crossover
 
 ### Usage
 # python ./src/PhaseCleaner_MergeAdjacents.py PStable
@@ -32,7 +33,9 @@ for i in range(1,len(PShead)):
 
 ### Looping, identifying mergeable adjacents, writing out either unmerged or merged
 for i in range(0,len(PSs)):
+	# If the SNP at the start of the phase block is not the first on the contig
 	if PSs[i][6] == 'No':
+		# write it to the output file
 		OutFile.write('\n'+PSs[i][0])
 		for k in range(1,len(PSs[i])):
 			OutFile.write('\t'+PSs[i][k])
